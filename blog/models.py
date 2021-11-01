24b73223ba5aa4fd3da5ac0f49b57cf6527215d4
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-11-01 22:30:16 +0300

    add model class for Blogview

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,18 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    
+    def save_location(self):
+        self.save()
+
+    def __str__(self):
+        return self.n
+
 class BlogView(models.Model):
   category = models.ForeignKey(Mycategory,null=True,on_delete=models.CASCADE)
   body=models.TextField(blank=True)
@@ -42,6 +54,39 @@ class BlogView(models.Model):
 
   def __str__(self):
         return self.name
+
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    occupants_count = models.IntegerField(default=0)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def create_neigborhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls, id):
+        cls.objects.filter(id=id).delete()
+
+    @classmethod
+    def update_neighbourhood(cls, id):
+        cls.objects.filter(id=id).update()
+
+    @classmethod
+    def search_by_name(cls, search_term):
+        hood = cls.objects.filter(name__icontains=search_term)
+        return hood
+
+    
+    @classmethod
+    def find_neigborhood(cls, id):
+        hood = cls.objects.get(id=id)
+        return hood
+
+    def __str__(self):
+        return self.name        
 
 class Mycategory(models.Model):
     name = models.CharField(max_length=100)
@@ -88,7 +133,7 @@ class Theprofile(models.Model):
         return self.name
 
 
-# business class model
+
 class Business(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
@@ -98,25 +143,24 @@ class Business(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # create business
+    
     def create_business(self):
         self.save()
 
-    # delete business
+   
     def delete_business(self):
         self.delete()
 
-    # update business
+    
     def update_business(self):
         self.update()
 
-    # search business
+    
     @classmethod
     def search_by_name(cls, search_term):
         business = cls.objects.filter(name__icontains=search_term)
         return business
 
-    # find business by id
     @classmethod
     def find_business(cls, id):
         business = cls.objects.get(id=id)
@@ -126,7 +170,7 @@ class Business(models.Model):
         return self.name
 
 
-# contact class model
+
 class Contact(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50, blank=True, null=True)
@@ -137,25 +181,25 @@ class Contact(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    # create contact
+    
     def create_contact(self):
         self.save()
 
-    # delete contact
+    
     def delete_contact(self):
         self.delete()
 
-    # update contact
+    
     def update_contact(self):
         self.update()
 
-    # search contact
+    
     @classmethod
     def search_by_name(cls, search_term):
         contact = cls.objects.filter(name__icontains=search_term)
         return contact
 
-    # find contact by id
+    
     @classmethod
     def find_contact(cls, id):
         contact = cls.objects.get(id=id)
@@ -163,3 +207,5 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name        
+
+      
