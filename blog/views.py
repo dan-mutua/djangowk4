61d4809221e1b4a413e9cd3597dea89944d5da6e
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-11-02 11:57:28 +0300

    add views

--- blog/views.py
+++ blog/views.py
@@ -78,3 +78,40 @@ def business(request):
         businesses = Business.objects.filter(
             neighbourhood=profile.neighbourhood)
         return render(request, "business.html", {"businesses": businesses})
+
+@login_required(login_url="/accounts/login/")
+def contacts(request):
+    current_user = request.user
+    # get current user neighbourhood
+    profile = Theprofile.objects.filter(user_id=current_user.id).first()
+    # check if user has neighbourhood
+    if profile is None:
+        profile = Theprofile.objects.filter(
+            user_id=current_user.id).first()  # get profile
+        posts = BlogView.objects.filter(user_id=current_user.id)
+        # get all locations
+        locations = Location.objects.all()
+        neighbourhood = NeighbourHood.objects.all()
+        category = Mycategory.objects.all()
+        businesses = Business.objects.filter(user_id=current_user.id)
+        contacts = Contact.objects.filter(user_id=current_user.id)
+        # redirect to profile with error message
+        return render(request, "profile.html", {"danger": "Update Profile by selecting Your Neighbourhood name to continue 😥!!", "locations": locations, "neighbourhood": neighbourhood, "categories": category, "businesses": businesses, "contacts": contacts, "posts": posts})
+    else:
+        neighbourhood = profile.neighbourhood
+        # get all contacts where the neighbourhood is the same as the user neighbourhood
+        contacts = Contact.objects.filter(
+            neighbourhood=profile.neighbourhood).order_by("-created_at")
+        return render(request, "contacts.html", {"contacts": contacts, "neighbourhood": profile.neighbourhood})        
+
+@login_required(login_url="/accounts/login/")
+def search(request):
+    if 'search_term' in request.GET and request.GET["search_term"]:
+        search_term = request.GET.get("search_term")
+        searched_businesses = Business.objects.filter(name__icontains=search_term)
+        message = f"Search For: {search_term}"
+
+        return render(request, "search.html", {"message": message, "businesses": searched_businesses})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, "search.html", {"message": message})
