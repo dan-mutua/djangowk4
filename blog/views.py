b3f51ff0ac86625022e0081a77b6b1d65e4ed49b
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-11-02 11:55:23 +0300

    add views

--- blog/views.py
+++ blog/views.py
@@ -53,3 +53,28 @@ def posts(request):
         # get all posts in the neighbourhood of the user ordered by date
         posts = BlogView.objects.filter(neighbourhood=neighbourhood).order_by("-created_at")
         return render(request, "posts.html", {"posts": posts})
+
+@login_required(login_url="/accounts/login/")
+def business(request):
+    current_user = request.user
+    # get current user neighbourhood
+    profile = Theprofile.objects.filter(user_id=current_user.id).first()
+    # check if user has neighbourhood
+    if profile is None:
+        profile = Theprofile.objects.filter(
+            user_id=current_user.id).first()  # get profile
+        posts = BlogView.objects.filter(user_id=current_user.id)
+        # get all locations
+        locations = Location.objects.all()
+        neighbourhood = NeighbourHood.objects.all()
+        category = Mycategory.objects.all()
+        businesses = Business.objects.filter(user_id=current_user.id)
+        contacts = Contact.objects.filter(user_id=current_user.id)
+        # redirect to profile with error message
+        return render(request, "profile.html", {"danger": "Update Profile by selecting Your Neighbourhood name to continue 😥!!", "locations": locations, "neighbourhood": neighbourhood, "categories": category, "businesses": businesses, "contacts": contacts, "posts": posts})
+    else:
+        neighbourhood = profile.neighbourhood
+        # get all businesses in the user neighbourhood
+        businesses = Business.objects.filter(
+            neighbourhood=profile.neighbourhood)
+        return render(request, "business.html", {"businesses": businesses})
